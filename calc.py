946082a89a0c695880536d90d94493d4726f8b81
Iblamesagar <sc462662@gmail.com>
2026-04-27 19:31:02 +0530

    Refactor calculator output formatting
    Added newline to output messages and improved formatting.

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-print("SIMPLE CALCULATOR")
+print("\nSIMPLE CALCULATOR")
 
 a = float(input("Enter the 1st number: "))
 b = float(input("Enter the 2nd number: "))
@@ -6,15 +6,15 @@ o = input("Enter the operator: ")
 
 if o == "+":
     c = a + b
-    print(f"Result: {a} + {b} = {c}")
+    print(f"Result: {a} + {b} = {c}\n")
 elif o == "*":
     c = a * b
-    print(f"Result: {a} * {b} = {c}")
+    print(f"Result: {a} * {b} = {c}\n")
 elif o == "-":
     c = a - b
-    print(f"Result: {a} - {b} = {c}")
+    print(f"Result: {a} - {b} = {c}\n")
 elif o == "/":
     c = a / b
-    print(f"Result: {a} / {b} = {c}")
+    print(f"Result: {a} / {b} = {c}\n")
 else:
     print(f"{o} is not a valid operator")
